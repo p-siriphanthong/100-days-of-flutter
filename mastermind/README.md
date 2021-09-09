748d35af309e46858bb697aab2611656f6fc18c0
Punn Siriphanthong <p.siriphanthong@gmail.com>
2021-09-10 03:06:36 +0700

    feat(mastermind): init project

---
+++ mastermind/README.md
@@ -0,0 +1,3 @@
+# Mastermind
+
+Color code breaking game
---
+++ mastermind/android/app/src/main/kotlin/com/example/mastermind/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.mastermind
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
